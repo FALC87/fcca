82b83ca5fc5230a1683e76293dda3e8baf4f86b6
EC2 Default User <ec2-user@ip-172-31-20-128.ec2.internal>
2023-06-21 23:38:15 +0000

    busqueda por fecha

--- fcca_datos/README.md
+++ fcca_datos/README.md
@@ -1,5 +1,12 @@
+Autores:
+- Dr. Oscar de la Torre
+- Dr. Felipe Andoni
+- MC. Rodolfo Lopez
+
+Descripcion:
+- Extraccion de datos del SNIIM.
+
+Requerimientos:
+- pip install MySQL-python
 
 
-
-
-pip install MySQL-python
--- fcca_datos/precios_sniim.py
+++ fcca_datos/precios_sniim.py
@@ -1,14 +1,14 @@
 import datetime
 import mysql.connector
-import logging
+#import logging
 import argparse
 import csv
 
 def parse_args():
     parser = argparse.ArgumentParser(description = 'Libreria para consulta de precios de productos agriculas FCCA UMICH V.1.1')
-    parser.add_argument('product',  help='Producto [fyh: frutas y hortalizas, maiz: maiz blanco y pozolero]', choices=['fyh', 'maiz'])
-    parser.add_argument('start_date', type=int, help='Fecha inicio [ddMMAA]')
-    parser.add_argument('end_date', type=int, help='Fecha fin [ddMMAA]')
+    parser.add_argument('product',    help='Producto [fyh: frutas y hortalizas, granos: granos]', choices=['fyh', 'granos'])
+    parser.add_argument('start_date', help='Fecha inicio [YYYY-mm-dd]')
+    parser.add_argument('end_date',   help='Fecha fin [YYYY-mm-dd]')
     args = parser.parse_args()
     return args
 
@@ -19,15 +19,19 @@ def main():
 def db_con(inputs):
     start_date = inputs.start_date
     end_date   = inputs.end_date
-    if inputs.product == 'maiz':
-        get_data('maiz', 'sniim_maiz_1', start_date,end_date)
+    if inputs.product == 'granos':
+        get_data('granos', 'sniim_maiz_1', start_date,end_date)
     if inputs.product == 'fyh':
         get_data('fyh', 'sniim_frutas_hortalizas_1', start_date, end_date)
 
 def get_data(file_name, product, start_date, end_date):
+    print('REPORTE EJECUTADO')
+    print('Fecha Inicial: ' + str(start_date))
+    print('Fecha Final: '   + str(end_date))
     mydb = mysql.connector.connect(host="18.215.228.120",user="fcca_read_1",password="fcca_read_1", port=3011, database="fcca_1")
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM "+str(product))
+    #mycursor.execute("SELECT * FROM "+ str(product) + " WHERE (STR_TO_DATE(fecha, '%d/%m/%Y') BETWEEN '2010-06-28' AND '2010-06-30') ")
+    mycursor.execute("SELECT * FROM "+ str(product) + " WHERE (STR_TO_DATE(fecha, '%d/%m/%Y') BETWEEN '" + str(start_date) + "' AND '" + str(end_date) + "') ")
 
     f = open('./data/'+str(file_name)+'_'+str(start_date)+'_'+str(end_date), 'w')
     writer = csv.writer(f, delimiter="|")
